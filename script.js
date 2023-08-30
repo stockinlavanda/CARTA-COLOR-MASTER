9ae8c6be5ab5a1011c37353bf6771f0d1fc3226a
Ailen Sampo <stockinlavanda@gmail.com>
2023-08-30 09:50:45 -0300

    primera prueba de responsive

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log("Hola desde JavaScript");
+const colorItems = document.querySelectorAll('.color-item');
+const totalAmount = document.getElementById('total-amount');
+
+colorItems.forEach(item => {
+  const quantityInput = item.querySelector('.quantity');
+  const price = parseFloat(item.getAttribute('data-price'));
+
+  quantityInput.addEventListener('input', updateTotal);
+
+  function updateTotal() {
+    let total = 0;
+    colorItems.forEach(item => {
+      const quantity = parseInt(item.querySelector('.quantity').value);
+      total += quantity * price;
+    });
+    totalAmount.textContent = total;
+  }
+});
