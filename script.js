d8e52d9b9bf06bd8671a25da893e1286714b2c67
Ailen Sampo <stockinlavanda@gmail.com>
2023-08-30 12:39:50 -0300

    se configuro formulario de envio de datos seleccionados

--- script.js
+++ script.js
@@ -17,3 +17,67 @@ colorItems.forEach(item => {
     totalAmount.textContent = total;
   }
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+  const enviarButton = document.getElementById("enviar");
+  const quantityInputs = document.querySelectorAll(".quantity");
+  
+  enviarButton.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    const detalles = [];
+    let totalAmount = 0;
+
+    quantityInputs.forEach(function(input) {
+      const parentCard = input.closest(".color-item");
+      const cardTitle = parentCard.querySelector(".card-title").textContent;
+      const cardText = parentCard.querySelector(".card-text").textContent;
+      const quantity = parseInt(input.value);
+      const price = parseFloat(parentCard.getAttribute("data-price"));
+      const subtotal = quantity * price;
+    });
+
+    //   if (quantity > 0) {
+    //     detalles.push({
+    //       nombre: cardTitle,
+    //       cantidad: quantity,
+    //       total: subtotal
+    //     });
+
+    //     totalAmount += subtotal;
+    //   }
+    // });
+
+    const nombre = document.querySelector('input[name="Nombre"]').value;
+    const apellido = document.querySelector('input[name="Apellido"]').value;
+    const telefono = document.querySelector('input[name="Telefono"]').value;
+
+    const plataformaCompra = document.querySelector('select[name="plataforma"]').value;
+
+    const dataToSend = {
+      nombre,
+      apellido,
+      telefono,
+      plataformaCompra,
+      detalles,
+      totalAmount
+    };
+
+    console.log("Data a enviar:", dataToSend);
+    fetch("https://formsubmit.co/stockinlavanda@gmail.com", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(dataToSend)
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log("Respuesta del servidor:", data);
+        // Puedes realizar acciones adicionales aquí en función de la respuesta del servidor
+      })
+      .catch(error => {
+        console.error("Error al enviar los datos:", error);
+      });
+  });
+});
